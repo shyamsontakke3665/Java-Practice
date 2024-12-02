5e86a49a867c5dbd44808c02aa2b599655f873e2
Shyam <shyamsontakke3665@gmail.com>
2024-12-02 23:16:54 +0530

    add new patterns

---
+++ src/NumberCharPattern/Pattern4.java
@@ -0,0 +1,20 @@
+package NumberCharPattern;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no");
+        int no = sc.nextInt();
+
+        for (int i = 1; i <= no; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/NumberCharPattern/Pattern5.java
@@ -0,0 +1,20 @@
+package NumberCharPattern;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no");
+        int no = sc.nextInt();
+        int count=1;
+
+        for(int i=1; i<=no; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
